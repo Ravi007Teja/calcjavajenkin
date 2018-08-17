a7dcd5a4cced7f02570a7036638be58e99a0a132
Ravi007Teja <ravi007teja.rt@gmail.com>
2018-08-17 12:46:08 +0530

    resolved useless parenthesis pmd warning

--- src/main/java/com/nissan/training/calculator/Calc.java
+++ src/main/java/com/nissan/training/calculator/Calc.java
@@ -2,15 +2,15 @@ package com.nissan.training.calculator;
 
 public class Calc {
 	public int add(int i,int j){
-	return(i+j);
+	return i+j;
 	}
 	
 	public int sub(int i,int j){
-	return(i-j);
+	return i-j;
 	}
 	
 	public int mul(int i,int j){
-	return(i*j);
+	return i*j;
 	}
 
 }
